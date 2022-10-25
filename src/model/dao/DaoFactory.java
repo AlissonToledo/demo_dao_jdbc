7cf9320691c054857335e1827653612029a8ffe3
Alisson Toledo <alisson.fptoledo.1990@gmail.com>
2022-10-25 09:21:11 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db.DB;
+import model.dao.Impl.DepartmentDaoJDBC;
 import model.dao.Impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao  creatSellerDao() {
 		return new SellerDaoJDBC(DB.getConnection());
+	}
+	
+	public static DepartmentDaoJDBC createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
 	}
 
 }
